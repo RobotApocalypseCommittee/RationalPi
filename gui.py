1ad160fdf77b6dfee05d73728f921d06e0320839
bhuvan21 <bhuvan.belur@gmail.com>
2017-10-03 17:34:48 +0100

    added literally nothing

--- gui.py
+++ gui.py
@@ -15,7 +15,7 @@ def test():
 
 #Initializes all the photos needed for the gui
 def initializePhotos():
-    global lockedBg, lockedButton, facial_guidance, placeholder, verificationBg, face_outline, instructions, take_photo, person_pic, cracker_pic
+    global lockedBg, lockedButton, facial_guidance, placeholder, verificationBg, face_outline, instructions, take_photo, person_pic, cracker_pic, hudBg
     lockedBg = PhotoImage(file=FILEPATH+"lockedScreen.gif")
     lockedButton = PhotoImage(file=FILEPATH+"unlockTest.png")
     placeholder = PhotoImage(file=FILEPATH+"placeholder.gif")
@@ -25,6 +25,7 @@ def initializePhotos():
     take_photo = PhotoImage(file=FILEPATH+"camera.png")
     person_pic = PhotoImage(file=FILEPATH+"personpic.gif")
     cracker_pic = PhotoImage(file=FILEPATH+"cracker.gif")
+    hudBg = PhotoImage(file=FILEPATH+"hudBg.png")
 
 #locked screen class
 class lockedScreen(Frame):
@@ -93,31 +94,37 @@ class hudScreen(Frame):
         self.master.title("Template as this will be fullscreen")
         crackers = {}
 
+        #background things
+        bg_lbl = Label(root, image=hudBg)
+        bg_lbl.image = hudBg
+        bg_lbl.place(x=0, y=0)
+
+
         #picture of respective person logged in
         person_lbl = Label(image=person_pic)
         person_lbl.image = person_pic
         person_lbl.grid(row=2, column=1, padx=20)
 
         #info label
-        info = Label(root, text="Yeeto Burrito", bg="white")
+        info = Label(root, text="Yeeto Burrito", bg="black", foreground="white")
         info.grid(row=2, column=2, padx=10)
 
         #lock button stuff
-        lock_button = Button(text="Lock")
-        lock_button.grid(row=1, column=3, padx=430, pady=20)
+        lock_button = Button(text="Lock", bg="black", activebackground = "black", fg="white")
+        lock_button.grid(row=1, column=5, padx=170, pady=15)
 
         #dispense button stuff
-        dispense_button = Button(text="Dispense")
-        dispense_button.grid(row=3, column=1, padx=20, pady=20)
+        dispense_button = Button(text="Dispense", bg="black", activebackground = "black", fg="white")
+        dispense_button.grid(row=3, column=2, padx=20, pady=20)
 
         #refill button things
-        refill_button = Button(text="Refill")
-        refill_button.grid(row=3, column=2, padx=20, pady=20)
+        refill_button = Button(text="Refill", bg="black", activebackground = "black", fg="white")
+        refill_button.grid(row=3, column=3, padx=20, pady=20)
 
         #crackers image things
         crackers = Label(image=cracker_pic)
         crackers.image = cracker_pic
-        crackers.grid(row=2, column=3, padx=20)
+        crackers.grid(row=2, column=4, padx=20)
 
 
 initializePhotos()
