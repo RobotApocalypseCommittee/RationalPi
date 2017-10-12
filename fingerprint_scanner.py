91cd5c3067bdc8ff32eff1d3639b351c2f919f8f
MrAttoAttoAtto <atto.allas@gmail.com>
2017-10-12 22:56:26 +0100

    Cleaning (and some fixing if i noticed it)

--- fingerprint_scanner.py
+++ fingerprint_scanner.py
@@ -1,8 +1,10 @@
-import enum
-import serial
 import binascii
+import enum
 import sys
 import time
+
+import serial
+
 
 class ResponsePacket:
     def __init__(self, data):
@@ -47,37 +49,43 @@ class FingerprintScanner:
         0x1010: "Capturing is cancelled",
         0x1011: "Invalid Parameter",
         0x1012: "Finger is not pressed",
-        
+
     }
     def __init__(self, port):
         self._ser = serial.Serial(port, baudrate=9600, timeout=2)
         self._open()
         self.led = False
-    
+
     def _open(self):
         retval = self._do_command(Command.OPEN)
+
         if not retval:
-            raise "Cannot open fingerprint sensor."
+            self.print_error("Cannot open fingerprint sensor.")
 
     def _revert_led(self):
         senddata = 1 if self.led else 0
         self._do_command(Command.CHANGE_LED, senddata)
-    
+
     def _do_command(self, command_id, data=0):
         command = bytearray([0x55, 0xAA, 0x01, 0x00])
         command.extend(data.to_bytes(4, byteorder='little'))
         command.extend(command_id.to_bytes(2, byteorder='little'))
         command.extend((sum(command)).to_bytes(2, byteorder='little'))
+
         if (self._ser.is_open):
             self._ser.write(command)
             response_data = self._ser.read(12)
             response = ResponsePacket(response_data)
+
             if (response.ok):
                 return response
+
             else:
                 return response # TODO: Add error code strings...
+
         else:
             self.print_error("Cannot write to serial, port closed.")
+
     def change_led(self, state = True, preserve=True):
         senddata = 1 if state else 0
         self.led = state if preserve else self.led
@@ -87,16 +95,15 @@ class FingerprintScanner:
         self.change_led(True, False)
         resp = self._do_command(Command.IS_PRESS_FINGER)
         self._revert_led()
-        if (resp.parameter == 0):
-            return True
-        else:
-            return False
+
+        return not bool(resp.parameter)
 
     def capture_finger(self, slow=True):
         senddata = 1 if slow else 0
         self.change_led(True, False)
         resp = self._do_command(Command.CAPTURE, senddata)
-        self._revert_led
+        self._revert_led()
+
         return resp
 
     def count_enrolled(self):
@@ -105,34 +112,46 @@ class FingerprintScanner:
 
     def determine_next_id(self):
         next_id = 20
+
         for i in range(20):
             if not self._do_command(Command.CHECK_ENROLLED, i).ok:
                 next_id = i
                 break
-        if id == 20:
+
+        if next_id == 20:
             self.print_error("No more space for people.")
+
         else:
             return next_id
 
-    
+
     def enroll_person(self):
         person_id = self.determine_next_id()
         self._do_command(Command.ENROLL_START, person_id)
+
         for i in range(3):
             self.change_led()
+
             while not self.is_finger_pressed(): time.sleep(0.1)
+
             resp = self.capture_finger()
+
             if not resp.ok:
                 print(resp.parameter)
+
             resp = self._do_command(Command.ENROLL1 + i)
+
             if not resp.ok:
                 print(resp.parameter)
+
             self.change_led(False)
+
             while self.is_finger_pressed(): time.sleep(0.1)
+
         return person_id
 
-    def delete_person(self, id):
-        resp = self._do_command(Command.DELETE_ID, id)
+    def delete_person(self, person_id):
+        resp = self._do_command(Command.DELETE_ID, person_id)
         return resp.ok
 
     def identify_person(self):
@@ -140,21 +159,16 @@ class FingerprintScanner:
         self.capture_finger()
         resp = self._do_command(Command.IDENTIFY)
         self._revert_led()
+
         if resp.ok:
             return resp.parameter
+
         else:
             return False
 
-    def print_error(self, error):
-        raise error
+    @staticmethod
+    def print_error(error):
+        raise FingerprintException(error)
 
     def close(self):
         self._ser.close()
-
-            
-
-
-
-
-
-
